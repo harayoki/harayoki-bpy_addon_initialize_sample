9c8be830fb19d5b5e39bbc0957cae7a317b2b2d6
harayoki <harayoki@harayoki.net>
2025-01-03 04:38:26 +0900

    何もしないディレクトリ型アドオンの追加

---
+++ addons/do_nothing_addon/__init__.py
@@ -0,0 +1,17 @@
+# 何もしないアドオン
+bl_info = {
+    "name": "Do Nothing Addon",
+    "blender": (3, 0, 0),
+    "category": "Object",
+}
+
+import bpy
+
+def register():
+    pass
+
+def unregister():
+    pass
+
+if __name__ == "__main__":
+    register()
